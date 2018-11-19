61b202b69430432214b6c1bf04156e3fa50f318b
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2018-11-19 11:31:21 +0200

    added movies, updated templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { MoviesComponent } from './movies/movies.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'movies', component: MoviesComponent },
   { path: '**', redirectTo: '' }
 ];
 
